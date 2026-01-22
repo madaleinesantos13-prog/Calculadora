15628f59c1be7de38e495045b76f56a610c7a8b3
madaleinesantos13-prog <madaleinesantos13@gmail.com>
2026-01-22 20:32:32 -0300

    Add files via upload

---
+++ calculadora.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+
+  # Inserir números
+x = float(input("Digite primeiro número:"))
+y = float(input("Digite segundo número:"))
+
+#Operações
+soma = x + y
+print ("A soma é",soma)
+subtração = x - y
+print ("A subtração é", subtração)
+divisão = x / y
+print ("A divisão é", divisão)
+multiplicação = x * y
+print ("A multiplicação é", multiplicação)
